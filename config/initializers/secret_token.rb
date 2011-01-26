799625c6be1c6aea2e7b7a2793418b4ea8e1dac9
Sergey A Reznikov <antisingle@gmail.com>
2011-01-26 22:43:48 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bux::Application.config.secret_token = 'df3e896e8721ba2319d38ec59e41aa66fce31873e9f5f9750e06e8fa76f108e00e2887605c51632a1a00d22eb50ca3085d486868b4ce143c863ecfb947f59544'
